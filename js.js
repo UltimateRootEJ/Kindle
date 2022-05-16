db0be40013c4a0d4276dbea1c1a537ca1dad0363
UltimateRootEJ <99173985+UltimateRootEJ@users.noreply.github.com>
2022-05-16 15:40:23 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -90,5 +90,74 @@ else
 
 
 
+//firebase
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCkLQ33d3VHPAE9utP-6gqD6xtUX0Oq3RA",
+  authDomain: "kindle-b1417.firebaseapp.com",
+  projectId: "kindle-b1417",
+  storageBucket: "kindle-b1417.appspot.com",
+  messagingSenderId: "500050668363",
+  appId: "1:500050668363:web:72be913f566e2c6f9456ce",
+  measurementId: "G-TH806S5N7G"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+firebase.analytics();
+
+// let's code 
+const datab  = firebase.database().ref('data');
+function UserRegister(){
+  if(names.value!='' && email.value!='' && surname.value!='' && passwrd.value!='' &&
+passwrdCon.value!=null && img.value!='')
+{ if(passwrd.value==passwrdCon.value)
+  {
+
+  const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+firebase.auth().createUserWithEmailAndPassword(email,password).then(
+  function(){
+   
+}).catch(function (error){
+   const errorcode = error.code;
+   const errormsg = error.message;
+});
+}}else
+}
+const auth = firebase.auth();
+function SignIn(){
+   const email = document.getElementById('username').value;
+   const password = document.getElementById('logPassword').value;
+   const logFirebase = auth.signInWithEmailAndPassword(email,password);
+   logFirebase.catch( e => alert(e.msg));
+   window.location.href = 'main_page.html';
+}
+document.getElementById('form').addEventListener('submit', (e) => {
+   e.preventDefault();
+   const userInfo = datab.push();
+   userInfo.set({
+       name: getId('fname'),
+       email : getId('eemail'),
+       password : getId('lpassword')
+   });
+   alert("Successfully Signed Up");
+   console.log("sent");
+   document.getElementById('form').reset();
+});
+function  getId(id){
+   return document.getElementById(id).value;
+}
+
+
+
+
 
 
